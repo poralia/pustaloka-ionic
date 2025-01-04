7a4c2d50a28bc433fcde3224de34bb8378fda838
Poralia <poraliaglobal@gmail.com>
2025-01-04 19:42:29 +0700

    enable capacitor http

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'id.pustaloka.app',
   appName: 'Pustaloka Ionic',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    "CapacitorHttp": {
+      "enabled": true,
+    }
+  }
 };
 
 export default config;
